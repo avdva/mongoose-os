57e2a805a1cb4f2c2e26039eb5c3ad6830298e9c
Aleksandr Demakin <alexander.demakin@gmail.com>
2017-03-30 00:51:38 +0300

    mos: show webview on linux.

--- mos/ui_linux.go
+++ mos/ui_linux.go
@@ -3,13 +3,28 @@ package main
 import (
 	"log"
 
-	"github.com/skratchdot/open-golang/open"
+	"github.com/miketheprogrammer/go-thrust/lib/bindings/window"
+	cmds "github.com/miketheprogrammer/go-thrust/lib/commands"
+	"github.com/miketheprogrammer/go-thrust/thrust"
 )
 
 func showUI(url string) {
-	if err := open.Start(url); err != nil {
+	thrust.InitLogger()
+	thrust.Start()
+	thrustWindow := thrust.NewWindow(thrust.WindowOptions{
+		RootUrl:  url,
+		HasFrame: true,
+		Size:     cmds.SizeHW{Width: 1280, Height: 720},
+		Title:    "Mongoose OS",
+	})
+	thrustWindow.Show()
+	thrustWindow.Focus()
+	stopCh := make(chan struct{})
+	_, err := thrustWindow.HandleEvent("closed", func(er cmds.EventResult, w *window.Window) {
+		stopCh <- struct{}{}
+	})
+	if err != nil {
 		log.Fatal(err)
 	}
-	// we've launched a http server, so wait forever.
-	<-(chan struct{})(nil)
+	<-stopCh
 }
